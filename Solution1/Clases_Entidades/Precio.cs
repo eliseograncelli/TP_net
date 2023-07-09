a83b9995cc87c07f47d03607fac900f7dafbbc7c
Eliseo Grancelli <eliseograncelli@gmail.com>
2023-07-09 20:19:41 -0300

    Inicio del proyecto

---
+++ Solution1/Clases_Entidades/Precio.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_Entidades
+{
+    internal class Precio
+    {
+        private int idProducto;
+        private float[] valor = new float[4];
+
+        public int IdProducto { get; set; }
+        public float[] Valor { get; set; } // El primer valor es precio unitario, el segundo valor es para entre 5 y 10
+                                           // productos, el tercer valor entre 10 y 20, y el cuarto +20u
+
+        public Precio(int idProducto, float[] valor)
+        {
+            this.IdProducto = idProducto;
+            this.Valor = valor;
+        }
+    }
+}
---
+++ Solution1/Clases_Entidades/Producto.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+
+namespace Clases_Entidades
+{
+    public class Producto
+    {
+        private static int ultimoID = 0;
+
+        private string nombre;
+        private int id;
+        private string descripcion;
+        private byte[] foto;
+
+        public string Nombre { get; set; }
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public byte[] Foto { get; set; }
+
+
+        public Producto(string nombre, string descripcion, byte[] foto)
+        {
+            this.Id = ultimoID++;
+            this.Nombre = nombre;
+            this.Descripcion = descripcion;
+            this.Foto = foto;
+        }
+    }
+} 
